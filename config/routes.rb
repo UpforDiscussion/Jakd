0851c8eccdc144287f5a8d5a7aaccd0405aaa1e0
Jonathan Ku <jonathanku88@gmail.com>
2012-12-01 13:01:22 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Jakd::Application.routes.draw do
+  
+  root :to => 'session#new'
+
+  resources :users
+  
+  resources :games
+
+  match '/login' => 'session#new', :via => :get
+  match '/logout' => 'session#destroy', :via => :get
+  match '/session' => 'session#create', :via => :post
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +59,7 @@ Jakd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
