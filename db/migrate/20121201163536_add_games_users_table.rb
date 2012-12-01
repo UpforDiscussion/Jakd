62682094aa58ee609a2e38e3d373588fbd7e83fd
Jonathan Ku <jonathanku88@gmail.com>
2012-12-01 16:43:18 -0500

    new

--- db/migrate/20121201163536_add_games_users_table.rb
+++
@@ -1,9 +0,0 @@
-class AddGamesUsersTable < ActiveRecord::Migration
-    def change
-        create_table :games_users, :id => false do |t|
-            t.integer :game_id
-            t.integer :user_id
-        
-        end
-    end
-end
